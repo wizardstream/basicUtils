6e2ea43022253dced393f97f2e727862ef2636bb
WizardStream <79329104+wizardstream@users.noreply.github.com>
2025-05-04 13:18:43 +0000

    Update basicUtils.py with colorama

--- basicUtils.py
+++ basicUtils.py
@@ -3,6 +3,7 @@ import time
 import sys
 from datetime import datetime
 import secrets
+from colorama import Fore, Back, Style
 
 now = datetime.now()
 
@@ -13,11 +14,13 @@ def greet():
     print("Hello, world!")
 
 def help():
-    print("""
+    print(Fore.GREEN + """
     -calc     -c          basicUtils -calc [add|sub|mul|div] num1 num2
     -maths
     
     -dateTime -dt         Prints time
+    
+    -echo     -e          echos
 
     -greet    -g          Greets the user
     
@@ -25,6 +28,7 @@ def help():
 
     -random   -rand       basicUtils -rand _num_   Generates random Hex number _num_ bytes big      
     """)
+    print(Fore.WHITE + "")
 def calc():
     if len(sys.argv) < 4:
         print("Missing arguments, try --help for help.")
@@ -35,7 +39,7 @@ def calc():
         a = float(sys.argv[3])
         b = float(sys.argv[4])
     except ValueError:
-        print("Error: operands must be numbers.")
+        print(Fore.RED + "Error:"+ Fore.WHITE +  " operands must be numbers.")
 
     if op == "add":
         print (a+b)
@@ -45,7 +49,7 @@ def calc():
         print(a*b)
     if op == "div":
         if b == 0:
-            print("Error: division by zero")
+            print(Fore.RED + "Error: " + Fore.WHITE  + "division by zero")
         else:
             print(a/b)
 
@@ -57,6 +61,12 @@ def HexRandom():
         print("Missing arguments, try --gelp for help.")
         return
     print(secrets.token_hex(int(sys.argv[2])))
+
+def echo():
+    if len(sys.argv) < 3:
+        print("")
+        return None
+    print(sys.argv[3])
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -75,5 +85,7 @@ if __name__ == "__main__":
         getTime()
     elif command == "-random" or command == "-rand":
         HexRandom()
+    elif command == "-echo" or command == "-e":
+        echo()
     else:
         print(f"Unknown command: {command}\nTry --help")
